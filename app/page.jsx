51b14f224fd2e5e7721c57a17bd7be2fc5f8bb73
mbuya <mbuyanemthokozisi@gmail.com>
2025-09-26 10:27:34 +0200

    url

--- app/page.jsx
+++ app/page.jsx
@@ -39,7 +39,7 @@ import Activitieshome from '@/components/Activitieshome';
 
 
 // Backend API base URL
-const API_BASE_URL = 'http://localhost:3001'; // Update with your backend URL
+const API_BASE_URL = 'https://project300-backend-qdzu.onrender.com'; // Update with your backend URL
 
 export default function Home() {
   const router = useRouter();
